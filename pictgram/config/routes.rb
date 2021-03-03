7712d9ef8531bd20a525f6e234e6c390823d8dea
suguru <ec2-user@ip-172-31-18-186.us-east-2.compute.internal>
2021-03-03 17:56:07 +0000

    linkページ

--- pictgram/config/routes.rb
+++ pictgram/config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
+  root 'pages#index'
   get 'pages/link'
 
-  root 'pages#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
 end
