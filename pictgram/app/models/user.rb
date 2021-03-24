67ffa0b391754de9b17379fc6d250fc0fcbb34dd
suguru <ec2-user@ip-172-31-18-186.us-east-2.compute.internal>
2021-03-24 01:55:18 +0000

    Rails 05

--- pictgram/app/models/user.rb
+++ pictgram/app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 15}
-  validates :email, presence: true
+  validates :email, presence: true,format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/}
+  validates :password, presence: true, length: { in: 8..32 },format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i}
   
-  VALID_PASSWORD_REGEX =   /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/
   has_secure_password
-  validates :password, presence: true,length: { in: 8..32 },format: { with: VALID_PASSWORD_REGEX}
+  
+  
 end
